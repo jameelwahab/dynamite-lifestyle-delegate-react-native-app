26f76463ea78af6021f45a4b63ce31f879a74ee0
Raza <aliraza@dynamiclogix.com>
2025-03-11 14:26:38 +0500

    added by ali

--- src/screens/Mission/List.js
+++ src/screens/Mission/List.js
@@ -99,7 +99,7 @@ const List = ({ navigation, route }) => {
 		const handlePress = (item) => ref.current.openModal?.(item)
 		const handleSelect = (opt,item)=>  {
 				if(opt.key=="copy") handleCopyMethod(item?.app_branch_url, item?._id, item?.type)
-				if(opt.key="member") nav.navigate(routes.missionMemberList, {item})
+				else if(opt.key="member") nav.navigate(routes.missionMemberList, {item})
 		} 
 
 	return (
