953a4bc777a86258f6f2763fd5cc0f879e3de02e
Raza <aliraza@dynamiclogix.com>
2025-03-28 11:22:05 +0500

    added by raza

--- src/screens/Affiliates/index.js
+++ src/screens/Affiliates/index.js
@@ -1,7 +1,7 @@
 import RootView from "../../components/RootView"
 import { GET_AFFILAITE_LINK } from "../../DAL"
 import { useState, useEffect } from "react"
-import { View, StyleSheet, FlatList, TouchableOpacity } from "react-native"
+import { View, StyleSheet, FlatList, TouchableOpacity, Pressable } from "react-native"
 import { useSelector } from 'react-redux'
 import { selectUser } from '../../redux/reducers/userSlice'
 import MyLoader from "../../components/MyLoader"
@@ -12,12 +12,14 @@ import {colors} from "../../utilities/colors"
 import {icons} from "../../utilities/icons"
 import {MyClearButton} from "../../components/MyButton"
 import copyText from "../../functions/copyText"
+import TitleView from "../../components/TitleView"
+import openUrl from "../../functions/openUrl"
 
 
 
 const Affiliates = ({navigation, route}) =>{
 
-		const { token } = useSelector(selectUser);
+		const { token, user } = useSelector(selectUser);
 		const [list, setList] =useState([])
 		const [loading, setLoading] = useState(false)
 		const [refreshing, setRefreshing] = useState(false)
@@ -31,13 +33,17 @@ const Affiliates = ({navigation, route}) =>{
 				}
 		}
 
+		console.log()
+
 		useEffect(()=>{
 				setLoading(true)
 				getList()
 		},[])
 
-		const renderView = (item)=>{
-				const copy= () => copyText(item.url, "Url copied to clipboard")
+		const renderView = (item, index)=>{
+				const affiliateLink = `${item.url}?affliate_url_name=${user.affiliate_url_name}`
+				const copy= () => copyText(affiliateLink, "Url copied to clipboard")
+
 				const copyLinkView = ()=> {
 						return (
 								<TouchableOpacity
@@ -51,8 +57,21 @@ const Affiliates = ({navigation, route}) =>{
 												>Copy Url</MyText>
 								</TouchableOpacity>
 						) }
+
+				const openPreview = ()=> {
+						return (
+								<TouchableOpacity
+										onPress={()=>openUrl(affiliateLink)}
+										style={{width:100,flexDirection:"row", alignItems:"center"}}>
+										{icons.goto()}
+								<MyText 
+										 fontSize={13} style={{ fontStyle:"italic", marginLeft:10 }} color={colors.primary} type='medium' >{"Preview"}</MyText>
+								</TouchableOpacity>
+						) }
+
 				return(
 						<View style={__styles.itemView}>
+								<MyText>{index + 1})</MyText>
 								<StatView 
 										title={"Name"}
 										value={item?.name}
@@ -62,13 +81,13 @@ const Affiliates = ({navigation, route}) =>{
 										value={item?.type}
 										/>
 								<StatView 
-										title={"URl"}
-										value={"Preview"}
-										/>
-								<StatView 
 										title={"Copy Link"}
 										value={"Yoko so"}
 										view={copyLinkView}
+										/>
+								<StatView 
+										title={"URL"}
+										view={openPreview}
 										/>
 						</View>
 				)
@@ -79,8 +98,14 @@ const Affiliates = ({navigation, route}) =>{
 				getList()
 		}
 
+		const titleView = () => {
+				return (
+						<TitleView  customStyle={{marginHorizontal:0}} title="Campaigns URL" original hideBackBottomButton />
+				)
+		}
+
 		return (
-				<RootView title="Campaign URL">
+				<RootView titleView={titleView} hideBackBottomButton>
 						<FlatList 
 								data={list}
 								contentContainerStyle={{paddingBottom:40}}
@@ -90,7 +115,7 @@ const Affiliates = ({navigation, route}) =>{
 										onRefresh={onRefresh}
 								/>}
 								KeyExtractor={item=> item?._id}
-								renderItem={({item})=> renderView(item)}
+								renderItem={({item, index})=> renderView(item,index)}
 						/>
 						<MyLoader enable={loading}/>
 				</RootView>
